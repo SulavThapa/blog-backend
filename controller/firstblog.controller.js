710ac1e8e3755bd80c1abb14fd75a1b20fcf2ebe
SulavThapa <46512631+SulavThapa@users.noreply.github.com>
2020-06-13 13:01:37 +0545

    Adde the Delete and Update function to the API

--- controller/firstblog.controller.js
+++ controller/firstblog.controller.js
@@ -41,3 +41,63 @@ exports.create = (req, res) =>{
     });
   });
 }
+
+//delete the note with the noteId
+exports.delete = (req, res) => {
+  FirstBlog.findByIdAndRemove(req.params.blogId)
+    .then(data => {
+      if(!data) {
+        return res.status(404).send({
+          message: "Blog not found with id " + req.params.blogId
+        });
+      }
+      res.send({message: "Bus deleted successfully!"});
+    }).catch(err => {
+    if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+      return res.status(404).send({
+        message: "Blog not found with id " + req.params.blogId
+      });
+    }
+    return res.status(500).send({
+      message: "Blog not delete Bus with id " + req.params.blogId
+    });
+  });
+};
+
+//to update the note with the noteId
+exports.update = (req, res) => {
+  // Validate Request
+  if(!req.body.id) {
+    return res.status(400).send({
+      message: "Blog content can not be empty"
+    });
+  }
+
+  // Find note and update it with the request body
+  FirstBlog.findByIdAndUpdate(req.params.blogId, {
+    id : req.body.id,
+    title: req.body.title,
+    name: req.body.name,
+    first_para: req.body.first_para,
+    second_para: req.body.second_para,
+    third_para: req.body.third_para,
+    fourth_para: req.body.fourth_para
+  }, {new: true})
+    .then(data => {
+      if(!data) {
+        return res.status(404).send({
+          message: "Blog not found with id " + req.params.blogId
+        });
+      }
+      res.send(data);
+    }).catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "Blog not found with id " + req.params.blogId
+      });
+    }
+    return res.status(500).send({
+      message: "Blog updating Driver with id " + req.params.blogId
+    });
+  });
+};
