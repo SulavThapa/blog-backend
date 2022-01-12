d36945da552487dfe52d4e34394af06136b8b28d
SulavThapa <46512631+SulavThapa@users.noreply.github.com>
2022-01-12 15:40:45 +0545

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://sulav:sulav21@cluster0-i1shl.mongodb.net/blog?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://sulav:<password>@cluster0-i1shl.mongodb.net/blog?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
